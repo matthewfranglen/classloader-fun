eb71cb9c54b0e4c3b4c90d99e491e05a22eee534
Matthew Franglen <matthew@franglen.org>
2017-08-21 22:33:23 +0100

    Add logging for loading
    Makes it easier to understand.

--- src/main/java/com/matthew/classloaderfun/FunClassLoader.java
+++ src/main/java/com/matthew/classloaderfun/FunClassLoader.java
@@ -1,12 +1,29 @@
 package com.matthew.classloaderfun;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FunClassLoader extends ClassLoader {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     public FunClassLoader() {
         super(FunClassLoader.class.getClassLoader());
     }
 
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        logger.info("Loading {}", name);
+
+        if (name.equals("java.lang.String")) {
+            return findClass(name);
+        } else {
+            return super.loadClass(name);
+        }
+    }
+
     protected Class<?> findClass(String name) throws ClassNotFoundException {
+        logger.info("Finding {}", name);
+
         return getParent().loadClass(
                 name
                     .replace("atomic", "kimota")
