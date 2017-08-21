5ce70a38baae84c180c98afbdc398f8ae1c44a97
Matthew Franglen <matthew@franglen.org>
2017-08-21 21:50:53 +0100

    start.spring.io
    Total 100% overkill. No regrets.

---
+++ src/main/java/com/matthew/classloaderfun/ClassloaderFunApplication.java
@@ -0,0 +1,12 @@
+package com.matthew.classloaderfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassloaderFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassloaderFunApplication.class, args);
+    }
+}
